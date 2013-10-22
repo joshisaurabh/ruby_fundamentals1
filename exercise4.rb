2c579d0093768197345c1eba982563da07808b51
joshisaurabh <reachme15@yahoo.com>
2013-10-22 15:56:33 -0400

    exercise 4 first commit

---
+++ exercise4.rb
@@ -0,0 +1,20 @@
+(1..100).each { |temp|
+	if temp%3 ==0
+		if temp%5==0
+			puts "Bitmaker"
+		else
+			puts "Bit"
+		end
+
+	elsif temp%5==0
+		if temp%3==0
+			puts "Bitmaker"
+		else
+			puts "maker"
+		end
+	elsif 
+		puts temp
+	end
+
+
+}
