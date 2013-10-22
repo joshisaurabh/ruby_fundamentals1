c264fffa1a83fb1c604bde1a9028e51b9c50c620
joshisaurabh <reachme15@yahoo.com>
2013-10-22 13:12:51 -0400

    Updated exercise1 file

--- exercise1.rb
+++ exercise1.rb
@@ -7,3 +7,22 @@ else
 	puts a + " " + b
 	
 end
+
+puts "Please enter first number"
+number1 = gets.chomp
+
+puts "Please enter second number"
+number2 = gets.chomp
+
+if number1 == number2
+	puts "Number 1 and Number 2 are equal"
+elsif number1 < number2
+	puts "Number 1 is less than Number 2"
+else
+	puts "Number 1 is greater than Number 2"
+end
+		
+
+
+	
+	
